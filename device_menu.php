8b8b434d58808b32b4de74c809ac3ea1618e1b80
Adrian Minde <Adrian_Minde@live.de>
2018-08-28 11:37:40 +0200

    Move device setup menu to profile

--- device_menu.php
+++ device_menu.php
@@ -3,7 +3,8 @@
     $domain = "messages";
     bindtextdomain($domain, "Modules/device/locale");
     bind_textdomain_codeset($domain, 'UTF-8');
-
+    
+    /* 
     $menu_dropdown_config[] = array(
             'name'=> dgettext($domain, "Device Setup"),
             'icon'=>'icon-home',
@@ -11,7 +12,8 @@
             'session'=>"write",
             'order' => 45
     );
-
+    */
+    
     $menu_dropdown[] = array(
             'name'=> dgettext($domain, "Things"),
             'icon'=>'icon-tasks',
