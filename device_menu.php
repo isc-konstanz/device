cff4659b371e572cca7251ccd77301f158504d48
Adrian Minde <Adrian_Minde@live.de>
2018-10-25 17:47:12 +0200

    Hide device menu entry only for beta view

--- device_menu.php
+++ device_menu.php
@@ -1,19 +1,21 @@
 <?php
+    global $user, $session;
 
     $domain = "messages";
     bindtextdomain($domain, "Modules/device/locale");
     bind_textdomain_codeset($domain, 'UTF-8');
-    
-    /* 
-    $menu_dropdown_config[] = array(
-            'name'=> dgettext($domain, "Device Setup"),
-            'icon'=>'icon-home',
-            'path'=>"device/view" ,
-            'session'=>"write",
-            'order' => 45
-    );
-    */
-    
+
+    $view = $user->get_preferences($session['userid'], 'deviceView');
+    if (!isset($view) || !$view) {
+         $menu_dropdown_config[] = array(
+             'name'=> dgettext($domain, "Device Setup"),
+             'icon'=>'icon-home',
+             'path'=>"device/view" ,
+             'session'=>"write",
+             'order' => 45
+         );
+    }
+
     $menu_dropdown[] = array(
             'name'=> dgettext($domain, "Things"),
             'icon'=>'icon-tasks',
